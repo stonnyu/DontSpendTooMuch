5c8c9172c3cd64add17bcd54fb71b4e5da6c50c1
Tonny <mrtonnysu@gmail.com>
2021-01-02 19:06:09 +0100

    Created spending class

---
+++ app/src/main/java/com/example/dontspendtoomuch/DataModel/Spending.kt
@@ -0,0 +1,27 @@
+package com.example.dontspendtoomuch.DataModel
+
+import com.google.firebase.database.DataSnapshot
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.properties.Delegates
+
+class Spending (snapshot: DataSnapshot?) {
+    lateinit var id: String
+    lateinit var spendingTitle: String
+    lateinit var spendingDate: Date
+    lateinit var spendingCategory: String
+    var spendingAmount by Delegates.notNull<Int>()
+
+    init {
+        try {
+            val data: HashMap<String, Any> = snapshot!!.value as HashMap<String, Any>
+            id = snapshot.key ?: ""
+            spendingTitle = data["spendingTitle"] as String
+            spendingDate = data["spendingDate"] as Date
+            spendingCategory = data["spendingCategory"] as String
+            spendingAmount = data["spendingAmount"] as Int
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
