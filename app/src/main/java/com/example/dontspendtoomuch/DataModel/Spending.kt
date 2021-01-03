43bd811f5a49613ebe4b9c1fa7ad2149f4c88f01
Tonny <mrtonnysu@gmail.com>
2021-01-03 13:56:50 +0100

    Added Add Spending functionality

--- app/src/main/java/com/example/dontspendtoomuch/DataModel/Spending.kt
+++ app/src/main/java/com/example/dontspendtoomuch/DataModel/Spending.kt
@@ -8,18 +8,18 @@ import kotlin.properties.Delegates
 class Spending (snapshot: DataSnapshot?) {
     lateinit var id: String
     lateinit var spendingTitle: String
-    lateinit var spendingDate: String
+    lateinit var spendingDate: Date
     lateinit var spendingCategory: String
-    var spendingAmount by Delegates.notNull<Long>()
+    var spendingAmount by Delegates.notNull<Double>()
 
     init {
         try {
             val data: HashMap<String, Any> = snapshot!!.value as HashMap<String, Any>
             id = snapshot.key ?: ""
             spendingTitle = data["spendingTitle"] as String
-            spendingDate = data["spendingDate"] as String
+            spendingDate = data["spendingDate"] as Date
             spendingCategory = data["spendingCategory"] as String
-            spendingAmount = data["spendingAmount"] as Long
+            spendingAmount = data["spendingAmount"] as Double
         } catch (e: Exception) {
             e.printStackTrace()
         }
--- app/src/main/java/com/example/dontspendtoomuch/ui/home/AddSpendingFragment.kt
+++ app/src/main/java/com/example/dontspendtoomuch/ui/home/AddSpendingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dontspendtoomuch.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,15 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.dontspendtoomuch.DataModel.CategoryModel
+import com.example.dontspendtoomuch.DataModel.Spending
+import com.example.dontspendtoomuch.R
 import com.example.dontspendtoomuch.databinding.FragmentAddSpendingBinding
 import com.example.dontspendtoomuch.databinding.FragmentCategoryBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
 import java.util.*
 
 private const val TAG: String = "AddSpendingFragment"
@@ -18,6 +25,8 @@ private const val TAG: String = "AddSpendingFragment"
 class AddSpendingFragment : Fragment() {
 
     private lateinit var binding: FragmentAddSpendingBinding
+    private var database = FirebaseDatabase.getInstance()
+    private var spendingReference = database.getReference("Spending")
     private var category: String? = null
 
     override fun onCreateView(
@@ -31,6 +40,45 @@ class AddSpendingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loadSpinner()
+
+        binding.btnAddSpending.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(requireContext())
+            alertDialog.setMessage("warning")
+            alertDialog.setNegativeButton("no") { _, _ -> }
+            alertDialog.setPositiveButton("yes") { _, _ ->
+                addSpending()
+                findNavController().navigate(R.id.navigation_home)
+            }
+            alertDialog.create().show()
+        }
+    }
+
+    private fun addSpending() {
+        val spending = Spending(null)
+        val newCategorySpending: DatabaseReference
+        val newSpending = spendingReference.child("").push()
+
+        spending.spendingTitle = binding.etSpendingTitle.text.toString()
+        spending.spendingDate = dateInputFormat()
+        spending.spendingAmount = binding.etSpendingAmount.text.toString().toDouble()
+
+        newSpending.child("spendingTitle").setValue(spending.spendingTitle)
+        newSpending.child("spendingDate").setValue(spending.spendingDate)
+        newSpending.child("spendingAmount").setValue(spending.spendingAmount)
+
+        newCategorySpending = database
+                .getReference("Category")
+                .child(category!!)
+                .child("spendings")
+        newCategorySpending.child(newSpending.key.toString()).setValue(spending.spendingTitle)
+    }
+
+    private fun dateInputFormat(): Date {
+        val dateString: String = binding.tilSpendingDay.editText?.text.toString() +
+                binding.tilSpendingMonth.editText?.text.toString() +
+                binding.tilSpendingYear.editText?.text.toString()
+
+        return SimpleDateFormat("dMyyyy").parse(dateString)
     }
 
     private fun loadSpinner() {
