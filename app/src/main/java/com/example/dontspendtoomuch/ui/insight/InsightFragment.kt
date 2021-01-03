efb1b481a65bf7a43e27b174e3c15c658f69c666
Tonny <mrtonnysu@gmail.com>
2021-01-03 12:46:15 +0100

    Changed color pallete and turned dashboard into insight tab

--- app/src/main/java/com/example/dontspendtoomuch/ui/insight/InsightFragment.kt
+++ app/src/main/java/com/example/dontspendtoomuch/ui/insight/InsightFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dontspendtoomuch.ui.dashboard
+package com.example.dontspendtoomuch.ui.insight
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dontspendtoomuch.R
 
-class DashboardFragment : Fragment() {
+class InsightFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var insightViewModel: InsightViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        insightViewModel =
+                ViewModelProvider(this).get(InsightViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_insight, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        insightViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
