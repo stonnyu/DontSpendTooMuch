9895916b19343c97f5c11567a1cb633a44ca1352
Tonny <mrtonnysu@gmail.com>
2021-01-04 23:31:17 +0100

    Added pie chart for expenses

--- app/src/main/java/com/example/dontspendtoomuch/ui/insight/InsightFragment.kt
+++ app/src/main/java/com/example/dontspendtoomuch/ui/insight/InsightFragment.kt
@@ -1,31 +1,63 @@
 package com.example.dontspendtoomuch.ui.insight
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.dontspendtoomuch.R
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.charts.Pie
+import com.example.dontspendtoomuch.DataModel.Category
+import com.example.dontspendtoomuch.DataModel.CategoryModel
+import com.example.dontspendtoomuch.DataModel.SpendingModel
+import com.example.dontspendtoomuch.databinding.FragmentInsightBinding
 
 class InsightFragment : Fragment() {
 
-    private lateinit var insightViewModel: InsightViewModel
+    private lateinit var binding: FragmentInsightBinding
+    private val categoriesData = CategoryModel.getData()!!
+    private val spendingsData = SpendingModel.getData()!!
+    private var spendingAmountSum = 0.0
+    private val map : HashMap<String, Double>  = hashMapOf()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-        insightViewModel =
-                ViewModelProvider(this).get(InsightViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_insight, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        insightViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentInsightBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initChart()
+    }
+
+    private fun initChart() {
+        var anyChartView: AnyChartView = binding.anyPiechartView
+        var dataEntries = arrayListOf<DataEntry>()
+        var pieChart: Pie = AnyChart.pie()
+
+        for (categories in categoriesData) {
+            spendingAmountSum = 0.0
+            for (spending in spendingsData) {
+                if (spending.spendingCategory == categories.categoryTitle) {
+                    spendingAmountSum += spending.spendingAmount
+                }
+
+            }
+            map.put(categories.categoryTitle, spendingAmountSum)
+        }
+
+        map.forEach {(key, value) ->
+            dataEntries.add(ValueDataEntry(key, value))
+        }
+
+        pieChart.data(dataEntries)
+        anyChartView.setChart(pieChart)
     }
 }
