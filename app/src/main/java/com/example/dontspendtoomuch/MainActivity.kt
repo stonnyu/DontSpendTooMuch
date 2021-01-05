bebd5bd11b214eaf70b3c8af6fbf2c482e8c2bca
Tonny <mrtonnysu@gmail.com>
2021-01-05 12:15:33 +0100

    Removed notification screen

--- app/src/main/java/com/example/dontspendtoomuch/MainActivity.kt
+++ app/src/main/java/com/example/dontspendtoomuch/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_insight, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_insight
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
